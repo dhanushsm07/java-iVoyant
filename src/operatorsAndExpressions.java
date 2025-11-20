75a0816f7d6fdeac7d5af1a74d1c40e3a99b2103
DhanushSMadivalar <dhanushsm619@gmail.com>
2025-11-20 16:37:04 +0530

    First Commit

---
+++ src/operatorsAndExpressions.java
@@ -0,0 +1,8 @@
+public class operatorsAndExpressions {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 0;
+            int c = a / b;  // division by zero error
+            System.out.println(c);
+        }
+}
