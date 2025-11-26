33d3149966d4d370abc1ae6541dc97efda50a18d
dhanushsm07 <dhanushm@ivoyant.com>
2025-11-26 23:33:11 +0530

    week-2 OOPs programs

---
+++ src/week_2/abstractionExample/AbstractionDemo.java
@@ -0,0 +1,11 @@
+package week_2.abstractionExample;
+
+public class AbstractionDemo {
+    public static void main(String args[]){
+        Animal dog1 = new Dog();
+
+        dog1.sound();
+        dog1.sleep();
+    }
+
+}
